5bc73115cd92debb2447064f1c7e4d2a7b7867ac
BALAJI VASI <balajivasi@gmail.com>
2023-06-17 17:54:44 -0400

    renamed the Project

--- ios/FloridaWICTests/FloridaWICTests.m
+++ ios/FloridaWICTests/FloridaWICTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface myAppTests : XCTestCase
+@interface FloridaWICTests : XCTestCase
 
 @end
 
-@implementation myAppTests
+@implementation FloridaWICTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
