6aeaf97a78d7e223ffdd7e7102b25390c73935f1
BALAJI VASI <balajivasi@gmail.com>
2023-06-17 16:48:36 -0400

    added UPC scan page

---
+++ src/Common/BarcodeScanner.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const BarcodeScanner = () => {
+
+    return (
+        <Text>Camera load function..</Text>
+    )
+}
+
+export default BarcodeScanner
