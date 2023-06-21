9e5ccfcae0373412f3e39805cfa092dfc32e8765
BALAJI VASI <balajivasi@gmail.com>
2023-06-21 17:37:21 -0400

    added UPC scanner to android.

--- src/Common/BarcodeScanner.js
+++ src/Common/BarcodeScanner.js
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
 import React from 'react';
-
+import { CameraScreen } from 'react-native-camera-kit';
 const BarcodeScanner = () => {
 
+    const onBarcodeScan = (qrValue) => {
+        console.log(qrValue)
+    };
     return (
-        <Text>Camera load function..</Text>
+        <CameraScreen
+            showFrame={false}
+            // Show/hide scan frame
+            scanBarcode={true}
+            // Can restrict for the QR Code only
+            laserColor={'blue'}
+            // Color can be of your choice
+            frameColor={'yellow'}
+            // If frame is visible then frame color
+            colorForScannerFrame={'black'}
+            // Scanner Frame color
+            onReadCode={(event) =>
+                onBarcodeScan(event.nativeEvent.codeStringValue)
+            }
+        />
     )
 }
 
