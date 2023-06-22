f9330d7f6b5b7656cada743f9f6648f780b6a24f
BALAJI VASI <balajivasi@gmail.com>
2023-06-22 12:06:59 -0400

    added fix for build issues

--- src/Common/WebView.js
+++ src/Common/WebView.js
@@ -1,8 +1,9 @@
-import React  from 'react'
-import { WebView } from 'react-native-webview';
+import React from 'react'
+//import { WebView } from 'react-native-webview';
+import RenderHtml from 'react-native-render-html';
 
-
-export const WebViewReader = (Data) =>{
-    const  HTMLContent=Data.Data;
-    return(<WebView source={{html:HTMLContent}} originWhitelist={['*']} className="text-xl" />);
+export const WebViewReader = (Data) => {
+    const HTMLContent = Data.Data;
+    //return(<WebView source={{html:HTMLContent}} originWhitelist={['*']} className="text-xl" />);
+    return (<RenderHtml source={{ html: HTMLContent }} className="text-xl" />);
 }
