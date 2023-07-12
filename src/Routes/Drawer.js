7e789dc301bfe8cb7264834fe8939f0f211488a7
BALAJI VASI <balajivasi@gmail.com>
2023-07-12 14:52:33 -0400

    added menu icons.

--- src/Routes/Drawer.js
+++ src/Routes/Drawer.js
@@ -15,6 +15,7 @@ import Profile from "../Common/Profile";
 import { logoutUser } from '../Services/authActions';
 import { useDispatch } from 'react-redux';
 import UPCScanStackNav from "./UPCScan";
+import { ArrowLeftOnRectangleIcon, ArrowUpTrayIcon, BuildingOfficeIcon, CalendarDaysIcon, CreditCardIcon, DocumentTextIcon, GlobeAltIcon, HomeIcon, LockClosedIcon, PencilIcon, PencilSquareIcon, ShoppingCartIcon } from "react-native-heroicons/outline";
 
 const Drawer = createDrawerNavigator();
 
@@ -32,19 +33,19 @@ const FLDrawer = () => {
 
     return (
         <Drawer.Navigator screenOptions={{ headerShown: false }} drawerContent={(props) => <CustomDrawer {...props} />} >
-            <Drawer.Screen name="Home" component={HomeStackNav} />
-            <Drawer.Screen name="Manage EBT Account" component={EBTCardStackNav} />
-            <Drawer.Screen name="Appointments" component={AppointmentsStackNav} />
-            <Drawer.Screen name="Benefits" component={BenefitsStackNav} />
-            <Drawer.Screen name="Clinics" component={ClinicsStackNav} />
-            <Drawer.Screen name="Stores" component={StoresStackNav} />
-            <Drawer.Screen name="Change Password" component={ChangePassStackNav} />
-            <Drawer.Screen name="Signatures" component={SignaturesStackNav} />
-            <Drawer.Screen name="Feedback" component={FeedbackStackNav} />
-            <Drawer.Screen name="Resource Links" component={ResourceStackNav} />
-            <Drawer.Screen name="Upload Documents" component={UploadDocumentsStackNav} />
+            <Drawer.Screen name="Home" component={HomeStackNav} options={{ drawerIcon: () => <HomeIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Manage EBT Account" component={EBTCardStackNav} options={{ drawerIcon: () => <PencilSquareIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Appointments" component={AppointmentsStackNav} options={{ drawerIcon: () => <CalendarDaysIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Benefits" component={BenefitsStackNav} options={{ drawerIcon: () => <CreditCardIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Clinics" component={ClinicsStackNav} options={{ drawerIcon: () => <BuildingOfficeIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Stores" component={StoresStackNav} options={{ drawerIcon: () => <ShoppingCartIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Change Password" component={ChangePassStackNav} options={{ drawerIcon: () => <LockClosedIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Signatures" component={SignaturesStackNav} options={{ drawerIcon: () => <DocumentTextIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Feedback" component={FeedbackStackNav} options={{ drawerIcon: () => <PencilIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Resource Links" component={ResourceStackNav} options={{ drawerIcon: () => <GlobeAltIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
+            <Drawer.Screen name="Upload Documents" component={UploadDocumentsStackNav} options={{ drawerIcon: () => <ArrowUpTrayIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
             <Drawer.Screen name="UPCScan" component={UPCScanStackNav} options={{ drawerItemStyle: { display: 'none' } }} />
-            <Drawer.Screen name="Logout" component={LogOut} />
+            <Drawer.Screen name="Logout" component={LogOut} options={{ drawerIcon: () => <ArrowLeftOnRectangleIcon name="fl-home" size={20} color={'red'} style={{ marginRight: -25 }} /> }} />
         </Drawer.Navigator>
     )
 }
